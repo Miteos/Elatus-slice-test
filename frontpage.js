4b71ee3c84b7c30f80e43ca23d64a1f57bc9a8b2
Matej <matej.kapular@gauss.hr>
2022-04-08 13:00:15 +0200

    feat: hamburger menu,responsive update

--- frontpage.js
+++ frontpage.js
@@ -2,13 +2,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     // check if DOM is ready
     let navbar = document.getElementById("nav");
-
     window.onscroll = ()=> {
         if (navbar !== null){
             window.scrollY > 100 ? navbar.classList.add("scroll") : navbar.classList.remove("scroll");
         }
-
     }
+
 });
 
 
